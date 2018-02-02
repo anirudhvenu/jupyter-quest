be36374a020a21c4ee23f707df7a0fd08efa8969
TheVenu <f2013355@pilani.bits-pilani.ac.in>
2018-02-02 14:59:47 +0800

    update gitignore

---
+++ src/store.js
@@ -0,0 +1,36 @@
+import { applyMiddleware, createStore, compose } from 'redux';
+import logger from 'redux-logger';
+import { reactReduxFirebase } from 'react-redux-firebase';
+import * as firebase from 'firebase';
+// import reducers
+import reducers from './reducers/';
+
+// import middleware
+import {firebaseLogger} from './helpers/'
+
+// firebase config
+const firebaseConfig = {
+  
+  apiKey: "AIzaSyCaXBvH4LEAO7fP7LNMMol1W8V9J8Fah8A",
+  authDomain: "jupyter-dev.firebaseapp.com",
+  databaseURL: "https://jupyter-dev.firebaseio.com",
+  projectId: "jupyter-dev",
+  storageBucket: "jupyter-dev.appspot.com",
+  messagingSenderId: "700109501520"
+
+};
+firebase.initializeApp(firebaseConfig);
+const config = {
+  userProfile: 'users',
+  enableLogging: false,
+  attachAuthIsReady: true,
+}
+
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, config),
+)(createStore)
+
+const store = createStoreWithFirebase(reducers, applyMiddleware(logger, firebaseLogger))
+
+
+export default store;
